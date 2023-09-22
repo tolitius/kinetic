71ee637812643aff4dd55451a2bf59f5362b9cdc
anatoly <tolitius@gmail.com>
2023-09-22 17:29:21 -0400

    dynamo: add describe & create table

--- src/kinetic/aws.clj
+++ src/kinetic/aws.clj
@@ -5,6 +5,14 @@
                                                     AwsCredentialsProvider]
            [software.amazon.awssdk.services.cloudwatch CloudWatchAsyncClient]
            [software.amazon.awssdk.services.dynamodb DynamoDbAsyncClient]
+           [software.amazon.awssdk.services.dynamodb.model DescribeTableRequest
+                                                           DeleteTableRequest
+                                                           CreateTableRequest
+                                                           KeySchemaElement
+                                                           KeyType
+                                                           AttributeDefinition
+                                                           ScalarAttributeType
+                                                           BillingMode]
            [software.amazon.awssdk.services.kinesis KinesisAsyncClient]
            [software.amazon.kinesis.common KinesisClientUtil]))
 
@@ -31,6 +39,43 @@
     :region  (.region (make-region region)) ;; region should be there
     creds    (.credentialsProvider (make-creds-provider creds))
     :and     .build))
+
+(defn describe-table [dynamo-db-client
+                      {:keys [table-name]}]
+  (let [request (-> (DescribeTableRequest/builder)
+                    (.tableName table-name)
+                    .build)]
+    (-> dynamo-db-client
+        (.describeTable request)
+        .get)))
+
+(defn create-table [dynamo-db-client
+                    {:keys [table-name
+                            key-schema
+                            attribute-definitions
+                            billing-mode]
+
+                     ;;TODO: make key schema and attribute definitions plural
+                     :or {key-schema {:key-type (KeyType/HASH)
+                                      :key-attribute "leaseKey"}
+                          attribute-definitions {:name "leaseKey"
+                                                 :type (ScalarAttributeType/S)}
+                          billing-mode (BillingMode/PAY_PER_REQUEST)}}]
+  (let [request (-> (CreateTableRequest/builder)
+                    (.tableName table-name)
+                    (.keySchema [(-> (KeySchemaElement/builder)
+                                     (.attributeName (:key-attribute key-schema))
+                                     (.keyType (:key-type key-schema))
+                                     .build)])
+                    (.attributeDefinitions [(-> (AttributeDefinition/builder)
+                                                (.attributeName (:name attribute-definitions))
+                                                (.attributeType (:type attribute-definitions))
+                                                .build)])
+                    (.billingMode billing-mode)
+                    .build)]
+    (-> dynamo-db-client
+        (.createTable request)
+        .get)))
 
 (defn make-cloud-watch-client [{:keys [region
                                        creds]}]
